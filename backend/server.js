bf12a1a507d8fc3d227839b02b0641267bd23325
mannatgupta146 <mannatgupta146@gmail.com>
2026-03-07 19:01:23 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const connectToDb = require('./src/config/db')
 
 connectToDb()
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`app is running on port ${PORT}`);
 });
