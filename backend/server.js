a23c38395f19eee4f14cf475a8212e3837284ae3
mannatgupta146 <mannatgupta146@gmail.com>
2026-03-07 19:04:10 +0530

    style: Refactor code formatting for consistency across server.js and app.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-require('dotenv').config();   // 👈 MUST be FIRST
+require("dotenv").config() // 👈 MUST be FIRST
 
-const app = require('./src/app')
-const connectToDb = require('./src/config/db')
+const app = require("./src/app")
+const connectToDb = require("./src/config/db")
 
 connectToDb()
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`app is running on port ${PORT}`);
-});
+  console.log(`app is running on port ${PORT}`)
+})
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,22 +1,24 @@
-const express = require('express')
-const cookieParser = require('cookie-parser')
+const express = require("express")
+const cookieParser = require("cookie-parser")
 
-const cors = require('cors')
+const cors = require("cors")
 
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true
-}))
+    credentials: true,
+  }),
+)
 
 //routes
-const authRoutes = require('./routes/auth.routes')
-const songRoutes = require('./routes/song.routes')
+const authRoutes = require("./routes/auth.routes")
+const songRoutes = require("./routes/song.routes")
 
-app.use('/api/auth', authRoutes)
-app.use('/api/songs', songRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/songs", songRoutes)
 
 module.exports = app
